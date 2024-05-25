4d79cd5ea2f8902fb5eb2488acb74af4de263d1b
Dasurf <damilareyusuf.dy@gmail.com>
2024-05-25 06:09:09 +0100

    modified and refactored my code to make it more reusable and using

--- App.jsx
+++ App.jsx
@@ -1,30 +1,39 @@
 import React from "react";
 import Joke from "./Joke";
+import JokesData from "./JokesData";
 
 export default function App() {
-
+    const jokeElements = JokesData.map((jokes) => {
+        return <Joke 
+            setup={jokes.setup}
+            punchline={jokes.punchline}
+        />
+    })
     return (
         <div>
-            <Joke 
-                setup="How did the hacker escape the police?"
-                punchline="He just ransomware!"
-            />
-            <Joke 
-                setup="I got my daughter a fridge for her birthday."
-                punchline="I can't wait to see her face light up when she opens it."
-            />
-            <Joke 
-                setup="Why don't pirates travel on mountain roads?"
-                punchline="Scurvy."
-            />
-            <Joke 
-                setup="Why do bees stay in the hive in the winter?"
-                punchline="Swarm"
-            />
-
-            <Joke 
-                punchline="I look in the mirror everyday in search for the best man alive and i keep seeing me"
-            />
+            {jokeElements}
         </div>
     )
 }
+
+
+{/* <Joke 
+    setup="How did the hacker escape the police?"
+    punchline="He just ransomware!"
+/>
+<Joke 
+    setup="I got my daughter a fridge for her birthday."
+    punchline="I can't wait to see her face light up when she opens it."
+/>
+<Joke 
+    setup="Why don't pirates travel on mountain roads?"
+    punchline="Scurvy."
+/>
+<Joke 
+    setup="Why do bees stay in the hive in the winter?"
+    punchline="Swarm"
+/>
+
+<Joke 
+    punchline="I look in the mirror everyday in search for the best man alive and i keep seeing me"
+/> */}
---
+++ JokesData.jsx
@@ -0,0 +1,25 @@
+export default [
+    {
+        setup: "How did the hacker escape the police?",
+        punchline: "He just ransomware!"
+    },
+    {
+        setup: "I got my daughter a fridge for her birthday.",
+        punchline: "I can't wait to see her face light up when she opens it."
+    },
+    {
+        setup: "Why don't pirates travel on mountain roads?",
+        punchline: "Scurvy"
+    },
+    {
+        setup: "Why do bees stay in the hive in the winter?",
+        punchline: "Swarm"
+    },
+    {
+        punchline: "I look in the mirror everyday in search for the best man alive and i keep seeing me"
+    },
+    {
+        setup: "Why do programmers prefer dark mode?",
+        punchline: "Because light attracts bugs!"
+    },
+]
